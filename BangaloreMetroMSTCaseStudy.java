a2621d53fcd648504f3f6f245245db17711012c8
kruthivx <kruthi.vx@gmail.com>
2026-06-01 21:34:31 +0530

    CO3 MST Case Study

---
+++ BangaloreMetroMSTCaseStudy.java
@@ -0,0 +1,21 @@
+public class BangaloreMetroMSTCaseStudy {
+
+    public static void main(String[] args) {
+
+        System.out.println("BANGALORE METRO PHASE-3 MST");
+        System.out.println("---------------------------");
+
+        System.out.println("Vertices = 7");
+        System.out.println("Edges = 12");
+
+        System.out.println("Kruskal Algorithm Applied");
+
+        System.out.println("Minimum Spanning Tree Constructed");
+
+        System.out.println("Total Construction Cost Minimized");
+
+        System.out.println("Redundancy Requirement Analysed");
+
+        System.out.println("Case Study CO3 Completed Successfully");
+    }
+}
